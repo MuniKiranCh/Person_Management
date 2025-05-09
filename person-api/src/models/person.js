978d892b16401ddead9df94846e6db8d46466c14
MuniKiranCh <125656756+MuniKiranCh@users.noreply.github.com>
2025-05-09 21:33:37 +0530

    added basic functionality

---
+++ person-api/src/models/person.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 100 },
+  age: { type: Number, required: true, min: 1, max: 120 },
+  gender: { type: String, required: true, enum: ["Male", "Female", "Other"] },
+  mobile: { type: String, required: true, match: /^\d{10}$/, unique: true },
+});
+
+module.exports = mongoose.model("Person", personSchema);
