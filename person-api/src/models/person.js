c60f5674eec3ceea511114ca19c3adc664b51dec
MuniKiranCh <125656756+MuniKiranCh@users.noreply.github.com>
2025-05-09 21:54:25 +0530

    added frontend

--- person-api/src/models/person.js
+++ person-api/src/models/person.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 100 },
+  email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
   age: { type: Number, required: true, min: 1, max: 120 },
   gender: { type: String, required: true, enum: ["Male", "Female", "Other"] },
   mobile: { type: String, required: true, match: /^\d{10}$/, unique: true },
